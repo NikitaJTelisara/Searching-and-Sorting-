6980113df41a102ea97a7d17747ac7f84438accc
NikitaJTelisara <nikitatelisara@gmail.com>
2018-03-07 16:24:59 -0600

    Update SearchingAndSorting.java

--- SearchingAndSorting.java
+++ SearchingAndSorting.java
@@ -45,13 +45,13 @@ public class SearchingAndSorting {
     }
 
     public static int[] doSelectionSort(int[] a) {
-        for (int i = 0; i <= a.length; i++) {
-            for (int j = i + 1; j < a.length; j++) {
-                if (a[i] > a[j]) {
-                    int smallerNumber = a[i];
-                    a[i] = a[j];
-                    a[j] = smallerNumber;
-                }
+        for (int i = 0; i < a.length-1; i++) {
+            for (int j = i+1;j <= a.length-1; j++) {
+                    if(a[i] > a[j]){
+                        int temp = a[i];
+                        a[i] = a[j];
+                        a[j] = temp;
+                    }
             }
         }
         return a;
