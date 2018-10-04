2b0783bf76c60d3c098c299af5e64918c48e4403
NikitaJTelisara <nikitatelisara@gmail.com>
2018-10-04 16:36:28 -0500

    Update SearchingAndSorting.java

--- SearchingAndSorting.java
+++ SearchingAndSorting.java
@@ -78,11 +78,11 @@ public class SearchingAndSorting {
 
     public static int[] doBubbleSort(int[] a) {
         for (int i = a.length - 1; i >= 0; i--) {
-            for (int j = 0; j < i; j++) {
-               /* if (a[j] > a[i]) { [j] and [j+1]  push highest element at the end
+            for (int j = 0; j < a.length - 1; j++) {
+                if (a[j] > a[j + 1]) {
                     int temp = a[j];
-                    a[j] = a[i];
-                    a[i] = temp;*/ 
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
                 }
             }
         }
